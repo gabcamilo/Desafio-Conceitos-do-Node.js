6ca15a9f255e6709c05326d4f02bdaae35b7d217
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-08-17 21:30:43 -0300

    :construction: Edit Repository

--- src/app.js
+++ src/app.js
@@ -40,7 +40,22 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs,} = request.body;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  if(repositoryIndex < 0){
+    return response.status(400).json({message: 'Repository not found'})
+  }
+
+  repositories[repositoryIndex]={
+    id,
+    title,
+    url,
+    techs,
+  }
+  
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
